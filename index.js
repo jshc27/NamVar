047109d457990c2ce4ff313ee7827392e849a3b2
Juan Herrera <juanchoherrerac_27@hotmail.com>
2021-06-22 14:36:34 -0500

    adding changes: finish

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { getNumberOfChars, getLower, getUpper } from "./operations.js";
 
 
+// Variables: Repuestas de cada metodo.
 
 var answer1 = document.querySelector('#answer1');
 var answer2 = document.querySelector('#answer2');
@@ -8,7 +9,7 @@ var answer3 = document.querySelector('#answer3');
 
 var name = document.querySelector('#your-name');
 
-name.addEventListener("keyup", () => {
+name.addEventListener("keyup", () => {  //Escuchar eventos
 
     answer1.textContent = getNumberOfChars(name.value);
     answer2.textContent = getLower(name.value);
--- operations.js
+++ operations.js
@@ -1,11 +1,17 @@
-export function  getNumberOfChars(name) {
+//Aplicacion de cada metodo. 
+
+
+// Cantidad de caracteres
+export function  getNumberOfChars(name) {  
     return name.length;
 }
 
+// Caracteres en minuscula 
 export function  getLower(name) {
     return name.toLowerCase();
 }
 
+// Caracteres en MAYUSCULA
 export function getUpper(name) {
     return name.toUpperCase();
 }
